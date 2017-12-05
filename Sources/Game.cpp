319eba0def24c4b2b2b62931a61a1e0d9bdbbcd7
hw16a118 <hw16a118@oecu.jp>
2017-12-05 18:27:24 +0900

    A_B_Cを実装

--- Sources/Game.cpp
+++ Sources/Game.cpp
@@ -1,9 +1,9 @@
 #include "Game.hpp"
 
 
-// TODO: 砲台の位置を画面左に、ターゲットの位置を画面右に移動させる。(A)
-// TODO: 雲の位置を左から右に動かす。見えなくなったら左端に戻す。(B)
-// TODO: 砲台を青い壁に沿って上下に動かす。(C)
+// TODO: 砲台の位置を画面左に、ターゲットの位置を画面右に移動させる。(A) HW16A118_谷口遼樹
+// TODO: 雲の位置を左から右に動かす。見えなくなったら左端に戻す。(B) HW16A118_谷口遼樹
+// TODO: 砲台を青い壁に沿って上下に動かす。(C) HW16A118_谷口遼樹
 // TODO: 弾のスピードを速くし、弾が画面右端を通り越したら再度発射可能にする。(D)
 // TODO: スコアのサイズを大きくする。(E)
 // TODO: スコアを100点ずつ加算するようにし、5桁の表示に変える。(F)
@@ -16,14 +16,14 @@ Vector2 cannonPos;      //!< 砲台の位置
 Vector2 bulletPos;      //!< 弾の位置
 Rect    targetRect;     //!< ターゲットの矩形
 int     score;          //!< スコア
-
+int     flag;
 
 // ゲーム開始時に呼ばれる関数です。
 void Start()
 {
     cloudPos = Vector2(-320, 100);
-    cannonPos = Vector2(-80, -150);
-    targetRect = Rect(80, -140, 40, 40);
+    cannonPos = Vector2(-310, -150);
+    targetRect = Rect(280, -140, 40, 40);
     bulletPos.x = -999;
     score = 0;
 }
@@ -54,6 +54,13 @@ void Update()
 
     // 雲の描画
     DrawImage("cloud1.png", cloudPos);
+    if (cloudPos.x > -999) {
+        cloudPos.x += 200 * Time::deltaTime;
+    }
+    if (cloudPos.x > 360) {
+        cloudPos = Vecotr2(-550, 100);
+        
+    }
 
     // 弾の描画
     if (bulletPos.x > -999) {
@@ -63,6 +70,18 @@ void Update()
     // 砲台の描画
     FillRect(Rect(cannonPos.x-10, -140, 20, 100), Color::blue);
     DrawImage("cannon.png", cannonPos);
+    if (cannonPos.y == -140) {
+        flag = 0;
+    }
+    if (cannonPos.y == -70) {
+        flag = 1;
+    }
+    if (flag == 1) {
+        cannonPos.y -= 1;
+    }
+    if (falg == 0) {
+        cannonPos.y += 1;
+    }
 
     // ターゲットの描画
     FillRect(targetRect, Color::red);
